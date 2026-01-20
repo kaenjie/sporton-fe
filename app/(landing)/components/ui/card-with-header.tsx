bf25abf58a57cabada5b961a4563ae9f8e693907
doyyseeean <mutiara_angelita_muhaeni@outlook.com>
2026-01-20 23:51:04 +0700

    backend integration

--- app/(landing)/components/ui/card-with-header.tsx
+++ app/(landing)/components/ui/card-with-header.tsx
@@ -5,12 +5,12 @@ type TCardWithHeaderProps = {
 
 const CardWithHeader = ({ title, children }: TCardWithHeaderProps) => {
   return (
-    <div className="bg-white">
+    <div className="bg-white ">
       <div className="px-5 py-4 border-b border-gray-200">
         <h2 className="font-bold text-lg">{title}</h2>
       </div>
       {children}
-    </div>  
+    </div>
   );
 };
 
